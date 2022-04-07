1615405e6563eb2984c2242458e9924402bac846
Keyvan Alonso <keyvanalonso@gmail.com>
2022-04-07 15:50:08 -0400

    commit tree b4ff8d4ccb72d176f8756d9369dc43be31f40f26

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+
+function moveDodgerLeft() {
+    //turn the current left location into a number only string
+    const leftDist = dodger.style.left.replace('px', '')
+    const left = parseInt(leftDist, 10)
+    console.log(left)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    const rightDist = dodger.style.left.replace('px', '')
+
+    const right = parseInt(rightDist, 10)
+    console.log(right)
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`
+    }
+}
+
+
+document.addEventListener('keydown', (e) =>{
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft()
+    } 
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight()
+    }
+
+})
+
+
+
